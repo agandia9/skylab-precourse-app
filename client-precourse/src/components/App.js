6dc8f926f8d4d3abc588897dae22ec7831f3deea
agandia9 <alejandro.gandia9@gmail.com>
2018-04-16 13:17:46 +0200

    added nav panel

--- client-precourse/src/components/App.js
+++ client-precourse/src/components/App.js
@@ -2,23 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navigator from './navigator/navigator.js'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Main from './main/main.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to dis thing.</h1>
+        <header className="App-header row">
+          <h1 className="App-title col-sm-10">Welcome to dis thing.</h1>
+          <ul className="col-sm-2">
+            <li>logout</li>
+            <li>profile</li>
+            <li>:)</li>
+          </ul>
         </header>
-        <div className="row">
-          <div className="col-sm-3">
-              <Navigator />
-          </div>
-          <div className="col-sm-9">
-              <Main />
-          </div>
+        <BrowserRouter>
+          <div className="row">
+            <div className="col-sm-3">
+                <Navigator />
+            </div>
+            <div className="col-sm-9">
+               <Route
+                 path={'/week/:nweek'}
+                 component={Main}
+                />
+            </div>
         </div>
+        </BrowserRouter>
       </div>
     );
   }
