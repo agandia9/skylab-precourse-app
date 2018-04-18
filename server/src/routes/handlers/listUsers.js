e19c33422a657f1e98d8f4233566c4d2ebab7248
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-04-18 16:48:18 +0200

    mini test in mocha

--- server/src/routes/handlers/listUsers.js
+++ server/src/routes/handlers/listUsers.js
@@ -1,7 +1,8 @@
+const { success, fail } = require('./api-utils')
 const logic = require('../../logic')
 
 module.exports = (req,res) => {
     logic.listUsers()
-        .then(users => res.json(users))
-        .catch(err => res.json(err))
+        .then(users => res.json(success(users)))
+        .catch(err => res.json(fail(err)))
 }
