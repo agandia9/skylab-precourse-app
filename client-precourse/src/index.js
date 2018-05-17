1b6a72bd589f6c57b2156fef45454dcad68afa0b
agandia9 <alejandro.gandia9@gmail.com>
2018-05-17 16:26:55 +0200

    ..?

--- client-precourse/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import './index.css';
-import Login from './login';
-
-ReactDOM.render(<Login />, document.getElementById('root'));
