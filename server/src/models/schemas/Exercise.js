e11be6be14b94c846b22a6d896e52c129d09fa04
Carlos Ramos <31883557+Asix94@users.noreply.github.com>
2018-06-27 10:26:47 +0200

    update

--- server/src/models/schemas/Exercise.js
+++ server/src/models/schemas/Exercise.js
@@ -3,21 +3,13 @@ const mongoose = require('mongoose')
 const { Schema, Schema: { ObjectId } } = mongoose
 
 module.exports = new Schema({
-    unit: {
+    unit : {
         type: ObjectId,
         ref: 'Subject',
         required: true
     },
-    index: {
-        type: String,
-        required: true
-    },
-    statement: {
-        type: String,
-        required: true
-    },
-    example: {
-        type: String,
-        required: true
-    }
+    index : String,
+    statement : String,
+    example : String
+    //status : Number
 })
--- server/src/models/schemas/Subject.js
+++ server/src/models/schemas/Subject.js
@@ -3,25 +3,13 @@ const mongoose = require('mongoose')
 const { Schema, Schema: { ObjectId } } = mongoose
 
 module.exports = new Schema({
-    unit : {
-        type: Number,
-        required: true
-    },
-    title : {
-        type: String,
-        required: true
-    },
-    theory : {
-        type: String,
-        required: true
-    },
-    resource : {
-        type: Array
-    },
+    unit : Number,
+    title : String,
+    theory : String,
+    resource : Array,
     exercises : [
         {
-            type: ObjectId, 
-            ref: 'Exercise'
+            type: ObjectId, ref: 'Exercise'
         }
     ]
 })
